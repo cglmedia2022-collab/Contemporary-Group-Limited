290b066fc6c32fa8b330558758ba82baede1aac8
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2026-07-05 10:21:30 +0100

    chore: remove unused database seeding and debugging scripts

--- checkDb.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const p = await mongoose.connection.collection('projects').findOne({ title: /Delta State University Of Science/i });
-  console.log(p);
-  process.exit(0);
-});
